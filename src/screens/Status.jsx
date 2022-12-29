74587bdb52c652f74e9df52ea2fe0d1af0657f43
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-29 21:56:34 +0500

    successfull

--- src/screens/Status.jsx
+++ src/screens/Status.jsx
@@ -7,16 +7,20 @@ export default function Status () {
     const images = useSelector(state => state.theme);
     
     let [counter,setCounter] = useState(0); 
+    let [width,setWidth] = useState(1)
 
        useEffect(()=>{
         setInterval(()=>{
             if(counter === images.length){
                 setCounter(0)
             }else {
-                setCounter(counter + 1)
+                setWidth(width + 1)
+                if(width === 1000){
+                    setCounter(counter + 1)
+                }
             }
             
-        },20000)
+        },10000)
        },[counter])
 
 
@@ -26,6 +30,9 @@ export default function Status () {
         <View style={styles.container}>
                 <View style={styles.first}>
                     <Text style={styles.firstText}>Your Beautifull Status</Text>
+                </View>
+                <View style={{width:width,height:4,backgroundColor:"red"}}>
+
                 </View>
                 <View style={styles.Second}>
                  <Image  style={styles.Image} source={{uri:images[counter]}}/>
